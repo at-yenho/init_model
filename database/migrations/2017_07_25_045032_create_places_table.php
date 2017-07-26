15c113033a4df986c6a3573a5a0ca838c3cd06a0
at-yenho <hothiyen30@gmail.com>
2017-07-26 09:44:39 +0700

    seed fatory 1

--- database/migrations/2017_07_25_045032_create_places_table.php
+++ database/migrations/2017_07_25_045032_create_places_table.php
@@ -17,6 +17,7 @@ class CreatePlacesTable extends Migration
             $table->increments("id");
             $table->string("name");
             $table->string("descript")->nullable();
+	    $table->string("parent_id")->nullable();
             $table->timestamps();
         });
     }
